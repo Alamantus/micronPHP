ca68252cd22483d780005b4b44adc00915f528b1
Robbie Antenesse <dev@alamantus.com>
2021-06-12 00:41:37 -0600

    Add simple templating structure

--- app/views/index.php
+++ app/views/index.php
@@ -1,2 +1,6 @@
-<h1> Hello <?php echo $name; ?></h1>
+<?php include_once('includes/header.php'); ?>
+
+<h1> Hello <?= $name; ?></h1>
 <h2>Welcome to Micron PHP. The simple framework for making simple web apps</h2>
+
+<?php include_once('includes/footer.php'); ?>
