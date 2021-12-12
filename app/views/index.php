f5ecd466feb488c26a66ad9e82056076815f798b
Robbie Antenesse <dev@alamantus.com>
2021-12-11 23:07:40 -0700

    Use micron function to load header & footer

--- app/views/index.php
+++ app/views/index.php
@@ -1,6 +1,6 @@
-<?php include_once('includes/header.php'); ?>
+<?php loadView('includes/header', [ 'page_title' => 'Home Page', 'description' => 'An example page.']); ?>
 
 <h1> Hello <?= $name; ?></h1>
 <h2>Welcome to Micron PHP. The simple framework for making simple web apps</h2>
 
-<?php include_once('includes/footer.php'); ?>
+<?php loadView('includes/footer'); ?>
